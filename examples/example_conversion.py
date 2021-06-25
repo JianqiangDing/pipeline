abd6937b2c8c830c4ffdcceb78f4c268515d7184
JianqiangDing <dingjianqiang0x@gmail.com>
2021-06-25 17:51:03 +0800

    label bugs of conversion module

--- examples/example_conversion.py
+++ examples/example_conversion.py
@@ -6,6 +6,7 @@ sys.path.append(os.getcwd())
 import pipeline as pl
 
 if __name__ == '__main__':
+    # TODO some visible artifacts (to be specifically, noise) exists currently, need to be fixed some day
     pl.cvt.images2gif('../results/20210625_171732')
     pl.cvt.images2video('../results/20210625_171732', '../results/video.mp4')
     pl.cvt.video2gif('../results/video.mp4', '../results/vid2gif.gif')
